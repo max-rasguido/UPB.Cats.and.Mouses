22ac2632a791a110bba4322aa58d16a3c17dcb37
max.rasguido <max.rasguido@gmail.com>
2012-10-26 21:45:53 -0400

    Added Classes
    Added Cat and Mouse class still need some work done

---
+++ GatosyRaton/GatosyRaton/Gato.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GatosyRaton
+{
+    class Gato
+    {
+        public int Posicionx
+        {
+            get;
+            set;
+        }
+        public int Posiciony
+        {
+            get;
+            set;
+        }
+        void Gato(int x, int y)
+        {
+            Posicionx = x;
+            Posiciony = y;
+        }
+
+        public int[] GetPos()
+        {
+            return new int[2]{Posicionx,Posiciony};
+        }
+
+        public bool MoveForward(bool Left)
+        {
+            if (Left && Posiciony < 8)
+            {Posiciony++;return true;}
+            else if (Posiciony < 8 && Posicionx < 4 && Posicionx > 1)
+            { Posicionx++; Posiciony++; return true; }
+            else
+                return false;
+        }
+    }
+}
---
+++ GatosyRaton/GatosyRaton/Raton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GatosyRaton
+{
+    class Raton : Gato
+    {
+        public bool MoveBackwards(bool Left)
+        {
+            return MoveForward(Left);
+        }
+
+        public override bool MoveForward(bool Left)
+        {
+            if (Left && Posiciony < 4)
+            { Posiciony--; return true; }
+            else if (Posiciony < 4 && Posicionx < 4 && Posicionx > 1)
+            { Posicionx++; Posiciony++; return true; }
+            else
+                return false;
+        }
+    }
+}
